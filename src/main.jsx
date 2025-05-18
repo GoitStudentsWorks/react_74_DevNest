3bfc946f059ab2d3a3343ea8b5ead44067eb5da4
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2025-05-18 21:40:04 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')).render(
 		<Provider store={store}>
 			<PersistGate loading={<Loader />} persistor={persistor}>
 				<UserProvider>
-					<BrowserRouter basename='/react_74_DevNest'>
+					<BrowserRouter>
 						<App />
 					</BrowserRouter>
 				</UserProvider>
